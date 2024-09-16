dd351503ee738045000a26e4184b4b984a38b892
SkRowson <rowson.aloha@gmail.com>
2024-09-16 21:16:48 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=2;
+
+        switch (a) {
+            case 0:
+            System.out.println("Case 0");
+            case 1:
+                System.out.println("Case 1");
+            case 2:
+                System.out.println("Case 2");
+                break;
+            case 3:
+                System.out.println("Case 3");
+            default:
+                System.out.println("Default");
+        }
+    }
+}
