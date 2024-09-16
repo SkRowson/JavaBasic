d6a6bf6323c83e9d5f7ddfec4df2e75968d13ff1
Sk Rowson Amed <rowson.aloha@gmail.com>
2024-09-16 21:21:12 +0530

    Comment update

--- Swap Two Numbers.java
+++ Swap Two Numbers.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         /* How do you swap two numbers without using a third variable in Java? */
 
-        // late A=10 & B=20
+        // declaration & initialization: A=10 & B=20
         int a=10, b=20;
 
         System.out.println("A="+a+" & B="+b);
